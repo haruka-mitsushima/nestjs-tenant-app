2d6694f7837777dd094bd3e87d704ffd35288ef6
Haruka Mitsushima <haruka-mitsushima@rakus-partners.co.jp>
2023-01-23 10:20:45 +0900

    item.service/controllerの実装

---
+++ src/item/dto/create-item.dto.ts
@@ -0,0 +1,22 @@
+import { Type } from 'class-transformer';
+import { IsInt, IsNotEmpty, IsString, Min } from 'class-validator';
+
+export class CreateItemDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsInt()
+  @Type(() => Number)
+  @IsNotEmpty()
+  @Min(1)
+  price: number;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsString()
+  @IsNotEmpty()
+  imageURL: string;
+}
--- src/item/item.controller.ts
+++ src/item/item.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
+import { Item } from '@prisma/client';
+import { CreateItemDto } from './dto/create-item.dto';
+import { UpdateItemDto } from './dto/update-item.dto';
+import { ItemService } from './item.service';
 
 @Controller('item')
-export class ItemController {}
+export class ItemController {
+  constructor(private readonly itemService: ItemService) { }
+
+  @Get()
+  async getItems(): Promise<Item[]> {
+    return await this.itemService.getItems();
+  }
+
+  @Post()
+  async createItem(@Body() dto: CreateItemDto): Promise<Item> {
+    return await this.itemService.createItem(dto);
+  }
+
+  @Post(':id')
+  async updateItemById(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() dto: UpdateItemDto,
+  ): Promise<Item> {
+    return await this.itemService.updateItemById(dto, id);
+  }
+}
