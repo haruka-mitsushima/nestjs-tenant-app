cdcaecf1b3efa0b8c1f51131181cb4a7e1ede3b6
Haruka Mitsushima <haruka-mitsushima@rakus-partners.co.jp>
2023-01-23 11:49:45 +0900

    class-transformerの修正

--- src/item/dto/delete-item.dto.ts
+++ src/item/dto/delete-item.dto.ts
@@ -1,4 +1,4 @@
-import { Transform, Type } from 'class-transformer';
+import { Transform } from 'class-transformer';
 import { IsBoolean, IsNotEmpty } from 'class-validator';
 
 export class DeleteItemDto {
